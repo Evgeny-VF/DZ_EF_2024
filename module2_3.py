132cacc8128bcbe0f3d83a812e40eefe90bba128
Evgeny-VF <168748597+Evgeny-VF@users.noreply.github.com>
2024-06-13 10:03:56 +0300

    Add files via upload

---
+++ module2_3.py
@@ -0,0 +1,25 @@
+#  Домашняя работа по уроку "Стиль кода часть II. Цикл While."
+my_list = my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+_listIndex = 0
+_trigger = 0
+while int(_listIndex) != int(len(my_list)):
+
+    while _listIndex <= int(len(my_list)) - 1:
+        if my_list[_listIndex] > 0:
+            print(my_list[_listIndex])
+            _listIndex = _listIndex + 1
+        elif my_list[_listIndex] < 0:
+            break
+        elif my_list[_listIndex] == 0:
+            _listIndex = _listIndex + 1
+            continue
+        if (int(len(my_list)) - int(_listIndex) - 1) == -1:
+            print('Проверка списка завершена.')
+
+        elif  int(my_list[_listIndex]) < 0:
+            print('Количество неидентифицированных элементов списка =  ', int(len(my_list)) - int(_listIndex) - 1)
+    if (int(len(my_list)) - int(_listIndex) - 1) != -1:
+        _trigger = input('Продолжить -"1", завершить - любой иной символ.')
+        if int(_trigger) == 1:
+            _listIndex = _listIndex + 1
+            continue
