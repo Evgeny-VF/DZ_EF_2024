4e6d99cd7ad173f2c05c99ee0fc203f87d07edfd
Evgeny-VF <168748597+Evgeny-VF@users.noreply.github.com>
2024-07-04 15:13:33 +0300

    Add files via upload

---
+++ module3_4.py
@@ -0,0 +1,12 @@
+# Самостоятельная работа по уроку "Произвольное число параметров".
+
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for word in other_words:
+        if (word.lower()).find(root_word.lower()) != -1 or (root_word.lower()).find(word.lower()) != -1:
+            same_words.append(word)
+    return same_words
+
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
